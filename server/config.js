d5ed1d50209c3f85152e2c16e26a68f74a8cfd73
vithanh173 <jhjmhn@gmail.com>
2023-11-24 19:31:03 +0700

    fix small thing

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 export const PORT = 7777;
 
-export const mongoDBURL = 'mongodb+srv://jhjmhn:SuuNhiVN123456@userdb.aa27oyp.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURL = "mongodb+srv://jhjmhn:SuuNhiVN123456@userdb.aa27oyp.mongodb.net/?retryWrites=true&w=majority";
 
 export const JWT_SECRET = "fjkahscnmasdqwpr15ds79c0safowqs";
